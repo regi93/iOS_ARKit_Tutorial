9a2e6e87e16ac7f84f40b9dc8d8fb3f9d2c3e829
you junyong <hellgey777@naver.com>
2022-04-14 16:54:30 +0900

    편의점 찾기 init

---
+++ WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/Secret.swift
@@ -0,0 +1,12 @@
+//
+//  Secret.swift
+//  WhereIsTheConvenienceStore
+//
+//  Created by 유준용 on 2022/04/14.
+//
+
+import Foundation
+
+let REST_API_KEY =  "0ab90c01bfe173c3f87b29ed0b9377d2"
+let NATIVE_APP_KEY = "e963099d499a5ad6ee055af65b340ec0"
+
---
+++ WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhereIsTheConvenienceStore
+//
+//  Created by 유준용 on 2022/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
