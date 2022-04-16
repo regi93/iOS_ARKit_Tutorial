e84916964ae45496ab671bafdf1138c750df1d7a
you junyong <hellgey777@naver.com>
2022-04-16 23:04:58 +0900

    Feat: Location Entity

---
+++ WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  WhereIsTheConvenienceStore
+//
+//  Created by 유준용 on 2022/04/16.
+//
+
+import Foundation
+
+struct DetailListCellData{
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
--- WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/LocationInformationViewController.swift
+++ WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/LocationInformationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LocationInformationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
