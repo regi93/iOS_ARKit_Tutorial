0613a7b75ed470ca9115e59c72fdf9b95926fda6
you junyong <hellgey777@naver.com>
2022-04-18 21:36:44 +0900

    Feat Viewmodel

--- WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/Presentation/LocationInformationViewController.swift
+++ WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/Presentation/LocationInformationViewController.swift
@@ -11,12 +11,93 @@ import RxCocoa
 import RxSwift
 
 class LocationInformationViewController: UIViewController {
-
+    let disposeBag = DisposeBag()
+    
+    let locationManager = CLLocationManager()
+    let mapView = MTMapView()
+    let currentLocationButton = UIButton()
+    let detailList = UITableView()
+    let viewModel = LocationInformationViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        bind(viewModel)
+        attribute()
+        layout()
     }
-
+    
+    private func bind(_ viewModel: LocationInformationViewModel){
+        currentLocationButton.rx.tap.bind(to: viewModel.currentLocationButtonTapped).disposed(by: disposeBag)
+        viewModel.setMapCenter.emit(to: )
+    }
+    private func attribute(){
+        title = "내 주변 편의점 찾기"
+        view.backgroundColor = .white
+        mapView.currentLocationTrackingMode = .onWithoutHeadingWithoutMapMoving
+        currentLocationButton.setImage(UIImage(systemName: "location.fill"), for: .normal)
+        currentLocationButton.layer.cornerRadius = 28
+    }
+    
+    private func layout(){
+        [mapView, currentLocationButton, detailList].forEach{
+            view.addSubview($0)
+        }
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            currentLocationButton.bottomAnchor.constraint(equalTo: detailList.bottomAnchor, constant: -20),
+            currentLocationButton.leadingAnchor.constraint(equalTo: detailList.leadingAnchor, constant: 20),
+            currentLocationButton.widthAnchor.constraint(equalToConstant: 40),
+            currentLocationButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            detailList.topAnchor.constraint(equalTo: mapView.bottomAnchor),
+            detailList.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            detailList.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            detailList.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
 }
 
+extension LocationInformationViewController: CLLocationManagerDelegate {
+    //사용자의 위치 얻기, 승인...
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // iOS 14+
+        let status = manager.authorizationStatus
+        switch status{
+        case .authorizedAlways, .authorizedWhenInUse, .notDetermined:
+            return
+        default:
+            viewModel.mapViewError.accept(MTMapViewError.locationAuthorizationDenied.errorDescription)
+            return
+        }
+    }
+}
+
+extension LocationInformationViewController: MTMapViewDelegate {
+    func mapView(_ mapView: MTMapView!, updateCurrentLocation location: MTMapPoint!, withAccuracy accuracy: MTMapLocationAccuracy) {
+        #if DEBUG
+        let defaultLocation = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.39, longitude: 127.110341))
+        viewModel.currentLocation.accept(location)
+        #else
+        viewModel.currentLocation.accept(location)
+        #endif
+    }
+    func mapView(_ mapView: MTMapView!, finishedMapMoveAnimation mapCenterPoint: MTMapPoint!) {
+        viewModel.mapCenterPoint.accept(mapCenterPoint)
+    }
+    func mapView(_ mapView: MTMapView!, selectedPOIItem poiItem: MTMapPOIItem!) -> Bool {
+        // POI : Pin표시된 Item
+        viewModel.selectPOIItem.accept(poiItem)
+        return false
+    }
+    func mapView(_ mapView: MTMapView!, failedUpdatingCurrentLocationWithError error: Error!) {
+        viewModel.mapViewError.accept(error.localizedDescription)
+    }
+}
--- WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/Presentation/LocationInformationViewModel.swift
+++ WhereIsTheConvenienceStore/WhereIsTheConvenienceStore/Presentation/LocationInformationViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import RxRelay
+import RxCocoa
+import RxSwift
+
+class LocationInformationViewModel{
+    let disposeBag = DisposeBag()
+    // viewModel -> view
+    let setMapCenter: Signal<MTMapPoint>
+    let errorMessage: Signal<String>
+    
+    // view -> viewModel
+    let currentLocation = PublishRelay<MTMapPoint>()
+    let mapCenterPoint = PublishRelay<MTMapPoint>()
+    let selectPOIItem = PublishRelay<MTMapPOIItem>()
+    let mapViewError = PublishRelay<String>()
+    let currentLocationButtonTapped = PublishRelay<Void>()
+    
+    
+    init (){
+//        지도 중심점 설정
+        let moveToCurrentLocation = currentLocationButtonTapped
+            .withLatestFrom(currentLocation)
+        let currentMapCenter = Observable.merge(
+            currentLocation.take(1),
+            moveToCurrentLocation
+        )
+        setMapCenter = currentMapCenter
+            .asSignal(onErrorSignalWith: .empty())
+        errorMessage = mapViewError.asObservable()
+            .asSignal(onErrorJustReturn: "잠시 후 다시 시도해주세요.")
+    }
+    
+    
+    
+}
